b36a22a5828abdc6adb4ad72fe45ce5571f9a043
Oak <mark@spire-web.co.uk>
2024-04-29 19:29:20 +0100

    implimented ring buffer for receipt, still missing the 3rd of 4 rapid i2c test block writes.

--- raspberry_pi/i2c_master.py
+++ raspberry_pi/i2c_master.py
@@ -4,4 +4,24 @@ import time
 bus = SMBus(1)
 chassis_address = 0x17;
 
-bus.write_byte_data(chassis_address, 0x00, 0x00)
+#bus.write_byte_data(chassis_address, 0x01, 0x02)
+
+b = [1, 2, 3, 4, 5, 6, 8] # there are 8
+bus.write_i2c_block_data(chassis_address, 0x01, b)
+
+#time.sleep(0.01)
+
+b = [1, 2, 3, 4, 5, 6, 8, 9] # there are 8
+bus.write_i2c_block_data(chassis_address, 0x02, b)
+
+time.sleep(0.01)
+
+b = [1, 2, 3, 4, 5, 6, 8, 9, 10] # there are 8
+bus.write_i2c_block_data(chassis_address, 0x03, b)
+
+#time.sleep(0.01)
+
+b = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11] # there are 8
+bus.write_i2c_block_data(chassis_address, 0x04, b)
+
+print("sent")
--- rp2/rp2.cpp
+++ rp2/rp2.cpp
@@ -1,27 +1,105 @@
+#include <hardware/i2c.h>
+#include <pico/i2c_slave.h>
+#include <pico/stdlib.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/i2c.h"
+#include <string.h>
 
-// I2C defines
-// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SLAVE_PORT i2c0
+#define RING_BUFFER_COUNT 8
+
+static const uint I2C_SLAVE_SDA_PIN = 16; // Green
+static const uint I2C_SLAVE_SCL_PIN = 17; // Yellow
+static const uint I2C_SLAVE_ADDRESS = 0x17;
+static const uint I2C_BAUDRATE = 100000; // 100 kHz
+
+static struct Context
+{
+    uint8_t mem_address;
+    bool mem_address_written = false;
+    uint8_t mem[256];
+    uint8_t bytes_received = 0;
+    bool final_received = false;
+    bool processed = true;
+} context;
+
+Context ring_buffer[RING_BUFFER_COUNT];
+uint8_t ring_buffer_position = 0;
+uint8_t next_process_position = 0;
+
+static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    switch (event) {
+    case I2C_SLAVE_RECEIVE: // master has written some data
+        if (!context.mem_address_written) {
+            ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
+
+            // writes always start with the memory address
+            uint8_t address = i2c_read_byte_raw(i2c);
+            context.mem_address = address;
+            context.mem_address_written = true;
+            context.bytes_received = 0;
+            context.final_received = false;
+        } else {
+            // save into memory
+            uint8_t data = i2c_read_byte_raw(i2c);
+            context.mem[context.bytes_received] = data;
+            context.bytes_received++;
+        }
+        break;
+    case I2C_SLAVE_REQUEST: // master is requesting data
+        break;
+    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+        context.mem_address_written = false;
+        context.final_received = true;
+        context.processed = false;
+
+        ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
+        ring_buffer[ring_buffer_position] = context;
+        break;
+    default:
+        break;
+    }
+}
+
+static void setup_slave() {
+    gpio_init(I2C_SLAVE_SDA_PIN);
+    gpio_pull_up(I2C_SLAVE_SDA_PIN);
+    gpio_set_function(I2C_SLAVE_SDA_PIN, GPIO_FUNC_I2C);
+
+    gpio_init(I2C_SLAVE_SCL_PIN);
+    gpio_pull_up(I2C_SLAVE_SCL_PIN);
+    gpio_set_function(I2C_SLAVE_SCL_PIN, GPIO_FUNC_I2C);
+
+    i2c_init(i2c0, I2C_BAUDRATE);
+    i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
+}
 
 int main()
 {
     stdio_init_all();
+    setup_slave();
 
-    // I2C Initialisation. Using it at 400Khz.
-    i2c_init(I2C_PORT, 400*1000);
-    
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
+    Context* current = &ring_buffer[next_process_position];
 
-    puts("Hello, world!");
+    while(1)
+    {
+        while(!current->processed)
+        {
+            current->processed = true;
+            current->final_received = false;
+            next_process_position = (next_process_position++ % RING_BUFFER_COUNT);
+            printf("Written %02X bytes, at address starting %02X\n", current->bytes_received, current->mem_address);
+            Context* current = &ring_buffer[next_process_position];
+        }
 
-    return 0;
+
+        //sleep_ms(3000);
+        /*
+        if(context.final_received)
+        {
+            // printf("Write at 0x%02X: '%s'\n", mem_address, msg);
+            printf("Written %02X bytes, at address starting %02X\n", context.bytes_received, context.mem_address);
+            context.final_received = false;
+        }
+        */
+    }
 }
