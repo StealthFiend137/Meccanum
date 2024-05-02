8ce457ec3ae77bb2aed09b99f9aefa8a448ac18c
Oak <mark@spire-web.co.uk>
2024-05-02 08:28:06 +0100

    read experimentation

--- raspberry_pi/i2c_master.py
+++ raspberry_pi/i2c_master.py
@@ -1,27 +1,42 @@
 from smbus import SMBus
 import time
 
+# Telemetry Registers
+CELL0 = 0x00
+CELL1 = 0x01
+CELL2 = 0x02
+
+# Movement Registers
+XDIR0 = 0x03
+XDIR1 = 0X04
+YDIR0 = 0x05
+YDIR1 = 0x06
+WDIR0 = 0x07
+WDIR1 = 0X08
+MOVET = 0x09
+
+# Sound Registers
+SOUND = 0x0A
+FREQ0 = 0x0B
+DUTY0 = 0x0D
+FREQ1 = 0x0D
+DUTY1 = 0x0E
+
+# Lighting Registers
+LIGH0 = 0x0F
+LIGH1 = 0x10
+
 bus = SMBus(1)
 chassis_address = 0x17;
 
 #bus.write_byte_data(chassis_address, 0x01, 0x02)
 
-b = [1, 2, 3, 4, 5, 6, 8] # there are 8
-bus.write_i2c_block_data(chassis_address, 0x01, b)
-
-#time.sleep(0.01)
-
-b = [1, 2, 3, 4, 5, 6, 8, 9] # there are 8
-bus.write_i2c_block_data(chassis_address, 0x02, b)
+#b = [1, 2, 3, 4, 5, 6, 8] # there are 8
+#bus.write_i2c_block_data(chassis_address, 0x01, b)
 
 time.sleep(0.01)
+adr = bus.read_i2c_block_data(chassis_address, CELL0, 3)
 
-b = [1, 2, 3, 4, 5, 6, 8, 9, 10] # there are 8
-bus.write_i2c_block_data(chassis_address, 0x03, b)
-
-#time.sleep(0.01)
-
-b = [1, 2, 3, 4, 5, 6, 8, 9, 10, 11] # there are 8
-bus.write_i2c_block_data(chassis_address, 0x04, b)
+print(adr)
 
 print("sent")
--- rp2/rp2.cpp
+++ rp2/rp2.cpp
@@ -7,10 +7,38 @@
 #define I2C_SLAVE_PORT i2c0
 #define RING_BUFFER_COUNT 8
 
+// Telemetry Registers (3)
+#define CELL0 0x00
+#define CELL1 0x01
+#define CELL2 0x02
+
+// Movement Registers (7)
+#define XDIR0 0x03
+#define XDIR1 0X04
+#define YDIR0 0x05
+#define YDIR1 0X06
+#define WDIR0 0x07
+#define WDIR1 0x08
+#define MOVET 0x09
+
+// Sound Registers (5)
+#define SOUND 0x0A
+#define FREQ0 0x0B
+#define DUTY0 0x0D
+#define FREQ1 0x0D
+#define DUTY1 0x0E
+
+// Lighting Registers (2)
+#define LIGH0 0x0F
+#define LIGH1 0x10
+
+// I2C
 static const uint I2C_SLAVE_SDA_PIN = 16; // Green
 static const uint I2C_SLAVE_SCL_PIN = 17; // Yellow
 static const uint I2C_SLAVE_ADDRESS = 0x17;
 static const uint I2C_BAUDRATE = 100000; // 100 kHz
+
+uint8_t registers[17][2];
 
 static struct Context
 {
@@ -28,35 +56,42 @@ uint8_t next_process_position = 0;
 
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
     switch (event) {
-    case I2C_SLAVE_RECEIVE: // master has written some data
-        if (!context.mem_address_written) {
-            ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
 
-            // writes always start with the memory address
-            uint8_t address = i2c_read_byte_raw(i2c);
-            context.mem_address = address;
-            context.mem_address_written = true;
-            context.bytes_received = 0;
-            context.final_received = false;
-        } else {
-            // save into memory
-            uint8_t data = i2c_read_byte_raw(i2c);
-            context.mem[context.bytes_received] = data;
-            context.bytes_received++;
-        }
-        break;
-    case I2C_SLAVE_REQUEST: // master is requesting data
-        break;
-    case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
-        context.mem_address_written = false;
-        context.final_received = true;
-        context.processed = false;
+        case I2C_SLAVE_RECEIVE: // master has initiated a connection
+            if (!context.mem_address_written) {
+                // reads and writes always start with the memory address
+                uint8_t address = i2c_read_byte_raw(i2c);
+                context.mem_address = address;
+                context.mem_address_written = true;
 
-        ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
-        ring_buffer[ring_buffer_position] = context;
-        break;
-    default:
-        break;
+                //ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
+                // context.bytes_received = 0;
+                // context.final_received = false;
+            } else {
+                // master is writing data
+                uint8_t data = i2c_read_byte_raw(i2c);
+                context.mem[context.bytes_received] = data;
+                context.bytes_received++;
+            }
+            break;
+
+        case I2C_SLAVE_REQUEST: // master is requesting data
+            // load from memory
+            i2c_write_byte_raw(i2c, registers[context.mem_address][1]);
+            context.mem_address++;
+            break;
+
+        case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
+            context.mem_address_written = false;
+            context.final_received = true;
+            context.processed = false;
+
+            // ring_buffer_position = (ring_buffer_position % RING_BUFFER_COUNT);
+            // ring_buffer[ring_buffer_position] = context;
+            break;
+
+        default:
+            break;
     }
 }
 
@@ -73,23 +108,35 @@ static void setup_slave() {
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
 }
 
+void registers_init()
+{
+    // Put some pretend values into the telemetry registers.
+    // battery cell registers are - 0-255
+    registers[CELL0][1] = 255;
+    registers[CELL1][1] = 128;
+    registers[CELL2][1] = 0;
+}
+
 int main()
 {
     stdio_init_all();
+
+    registers_init();
+
     setup_slave();
 
-    Context* current = &ring_buffer[next_process_position];
+    // Context* current = &ring_buffer[next_process_position];
 
     while(1)
     {
-        while(!current->processed)
-        {
-            current->processed = true;
-            current->final_received = false;
-            next_process_position = (next_process_position++ % RING_BUFFER_COUNT);
-            printf("Written %02X bytes, at address starting %02X\n", current->bytes_received, current->mem_address);
-            Context* current = &ring_buffer[next_process_position];
-        }
+        // while(!current->processed)
+        // {
+        //     current->processed = true;
+        //     current->final_received = false;
+        //     next_process_position = (next_process_position++ % RING_BUFFER_COUNT);
+        //     printf("Written %02X bytes, at address starting %02X\n", current->bytes_received, current->mem_address);
+        //     Context* current = &ring_buffer[next_process_position];
+        // }
 
 
         //sleep_ms(3000);
