a02b775d615f58e0bdbe6ba217ada6d7fa402ea8
Oak <mark@spire-web.co.uk>
2024-05-25 21:20:58 +0100

    addeed enum for movement modifications

--- rp2/chassis/chassis.cpp
+++ rp2/chassis/chassis.cpp
@@ -1,12 +1,17 @@
 #include "chassis.h"
 #include <pico/stdlib.h>
+#include <vector>
 
-/// Public
-
+/// @brief Creates a new instance of the Chassis class.
+/// @param comamnd_timeout_ms The timeout in ms before items return to their default state.
 Chassis::Chassis(uint comamnd_timeout_ms) : _command_timeout_ms(comamnd_timeout_ms)
 {
 };
 
+/// @brief Sets all of the movement axes simultaneously.
+/// @param xVelocity The value of the x axis.
+/// @param yVelocity The value of the y axis.
+/// @param wVelocity The value of the w axis.
 void Chassis::set_all_axis(int xVelocity, int yVelocity, int wVelocity)
 {
     int start_time = to_ms_since_boot(get_absolute_time());
@@ -50,25 +55,41 @@ void Chassis::set_w_axis(int velocity)
     set_axis(&wAxis, velocity);
 };
 
-/// @brief Gets the value of the w axis.
+/// @brief Gets the value of the w axis.c++ 
 /// @return Returns the value of the w axis.
 int Chassis::get_w_axis()
 {
     return get_axis(&wAxis);
 };
 
-/// Private
+void Chassis::register_callback(void(*callback)())
+{
+    this->_modificationCallbacks.push_back(callback);
+};
 
+/// @brief Private method to set the speed of a provided axis.
+/// @param movementAxis The axis to set.
+/// @param speed The speed to set the axis to.
 void Chassis::set_axis(MovementAxis* movementAxis, int speed)
 {
     movementAxis->set_speed(speed);
+    // ModificationFlags flags = static_cast<ModificationFlags>(
+    //     xAxisModified | yAxisModified | wAxisModified
+    // );
 };
 
-void Chassis::set_axis(MovementAxis* movementAxis, int speed, int start_time)
+/// @brief Private method to set the speed of a provided axis at a specified time.
+/// @param movementAxis The axis to set.
+/// @param speed The speed to set the axis to.
+/// @param update_time The time to record as when this change was made.
+void Chassis::set_axis(MovementAxis* movementAxis, int speed, int update_time)
 {
-    movementAxis->set_speed(speed, start_time);
+    movementAxis->set_speed(speed, update_time);
 };
 
+/// @brief Private method to get the speed of a specified axis.
+/// @param MovementAxis The axis to get the speed of.
+/// @return Returns the speed of the provided axis.
 int Chassis::get_axis(MovementAxis* MovementAxis)
 {
     return MovementAxis->get_speed();
--- rp2/chassis/chassis.h
+++ rp2/chassis/chassis.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <vector>
 #include "movementAxis.h"
 
+/// @brief Control and monitoring of the meccanum chassis.
 class Chassis
 {
 public:
@@ -9,6 +11,10 @@ public:
     /// @param comamnd_timeout_ms The amount of time in milliseconds until a command is cancelled. The time is reset every time it is refreshed.
     Chassis(uint comamnd_timeout_ms);
 
+    /// @brief Sets all of the movement axes simultaneously.
+    /// @param xVelocity The value of the x axis.
+    /// @param yVelocity The value of the y axis.
+    /// @param wVelocity The value of the w axis.
     void set_all_axis(int xVelocity, int yVelocity, int wVelocity);
 
     /// @brief Sets the speed of the axis.
@@ -35,16 +41,45 @@ public:
     /// @return Returns the speed of the axis. 
     int get_w_axis();
 
+    void register_callback(void(*callback)());
+
+    enum ModificationFlags
+    {
+        xAxisModified = 1 >> 0,
+        yAxisModified = 1 >> 1,
+        wAxisModified = 1 >> 2
+    };
+
 private:
 
+    /// @brief The x axis.
     MovementAxis xAxis;
+
+    /// @brief The y axis.
     MovementAxis yAxis;
+
+    /// @brief The w axis.
     MovementAxis wAxis;
+
+    /// @brief Vector of callbacks used to notify of changes to chassis values.
+    std::vector<void (*)()> _modificationCallbacks;
 
     /// @brief Backing field for the amount of time in milliseconds without renewall until an action is stopped.
     uint _command_timeout_ms;
 
+    /// @brief Private method to set the speed of a provided axis.
+    /// @param movementAxis The axis to set.
+    /// @param speed The speed to set the axis to.
     void set_axis(MovementAxis* movementAxis, int speed);
+
+    /// @brief Private method to set the speed of a provided axis at a specified time.
+    /// @param movementAxis The axis to set.
+    /// @param speed The speed to set the axis to.
+    /// @param update_time The time to record as when this change was made.
     void set_axis(MovementAxis* movementAxis, int speed, int start_time);
+
+    /// @brief Private method to get the speed of a specified axis.
+    /// @param MovementAxis The axis to get the speed of.
+    /// @return Returns the speed of the provided axis.
     int get_axis(MovementAxis* MovementAxis);
 };
